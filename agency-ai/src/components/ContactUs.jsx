a1a4b13e7a277551a69cf638cd715078dad83450
ZahraQaisar <zqaisar427@gmail.com>
2025-09-09 06:27:23 +0500

    updated

--- agency-ai/src/components/ContactUs.jsx
+++ agency-ai/src/components/ContactUs.jsx
@@ -10,30 +10,30 @@ const ContactUs = () => {
         event.preventDefault();
         const formData = new FormData(event.target);
         formData.append("access_key", "4160c6f9-513c-47c7-a405-6daa95113110");
-        try{
-const response = await fetch("https://api.web3forms.com/submit", {
-            method: "POST",
-            body: formData
-        });
-        const data = await response.json();
-        if (data.success) {
-            toast.success('Thank you for your submission!')
-            event.target.reset();
-        } else {
-            toast.error(data.message)
-        }
-        }catch(error){
-toast.error(error.message)
+        try {
+            const response = await fetch("https://api.web3forms.com/submit", {
+                method: "POST",
+                body: formData
+            });
+            const data = await response.json();
+            if (data.success) {
+                toast.success('Thank you for your submission!')
+                event.target.reset();
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
         }
 
     }
 
     return (
         <motion.div
-        initial="hidden"
-        whileInView="visible"
-        transition={{ staggerChildren: 0.2}}
-        viewport={{ once: true }}
+            initial="hidden"
+            whileInView="visible"
+            transition={{ staggerChildren: 0.2 }}
+            viewport={{ once: true }}
             id='contact-us'
             className='flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-28 text-gray-700 dark:text-white'
         >
@@ -41,12 +41,12 @@ toast.error(error.message)
                 title='Reach out to us'
                 desc='From strategy to execution, we craft digital solutions that move your business forward.'
             />
-            <motion.form 
-            initial={{ opacity: 0, y: 30 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: 0.4}}
-            viewport={{ once: true }}
-            onSubmit={onSubmit} className='grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full'>
+            <motion.form
+                initial={{ opacity: 0, y: 30 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5, delay: 0.4 }}
+                viewport={{ once: true }}
+                onSubmit={onSubmit} className='grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full'>
                 <div>
                     <p className='mb-2 text-sm font-medium'>Your name</p>
                     <div className='flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600'>
--- agency-ai/src/components/Teams.jsx
+++ agency-ai/src/components/Teams.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import Title from './Title'
-import { teamData } from '../assets/assets' 
+import { teamData } from '../assets/assets'
 import { motion } from 'motion/react'
 
 const Teams = () => {
     return (
         <motion.div
-        initial="hidden"
-        whileInView="visible"
-        viewport={{once:true}}
-         className='flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-28 text-gray-800 dark:text-white'>
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            className='flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-28 text-gray-800 dark:text-white'>
             <Title
                 title='Meet the team'
                 desc="A passionate team of digital experts is dedicated to your brand's success."
@@ -17,10 +17,10 @@ const Teams = () => {
             <div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5'>
                 {teamData.map((team, index) => (
                     <motion.div
-                    initial={{ opacity: 0, y: 20 }}
-                    whileInView={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.4, delay:index*0.1}}
-                    viewport={{ once: true }}
+                        initial={{ opacity: 0, y: 20 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.4, delay: index * 0.1 }}
+                        viewport={{ once: true }}
                         key={index}
                         className='flex max-sm:flex-col items-center gap-5 p-4 rounded-xl 
                                    border border-gray-100 dark:border-gray-700 bg-white 
